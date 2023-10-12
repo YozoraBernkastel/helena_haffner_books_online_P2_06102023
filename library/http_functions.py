8f571bedbd7a4d24bf8d477a2e882141f7356075
Featherine <helaena.ha2fner@gmail.com>
2023-10-12 19:05:12 +0200

    04 routeur using to define which type of page was given by the user

--- library/http_functions.py
+++ library/http_functions.py
@@ -1,10 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
 
 def check_url(url) -> bool:
     is_url_exist = url.status_code == 200
-
-    if is_url_exist:
-        print("This url is valid")
-    else:
-        print("ERROR 404 -> This page doesn't exist")
-
     return is_url_exist
+
+
+def url_type(url):
+    if url == "https://books.toscrape.com/index.html":
+        return "home"
+    else:
+        soup = BeautifulSoup(requests.get(url).text, 'html5lib')
+        if soup.find("ul", class_="nav nav-list"):
+            return "category"
+        else:
+            return "book"
--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
-from library.scrap_book_function import *
-from library.scrap_category import scrap_all_category_books
+from library.srap_data_routeur import scrap_datas
 
 
 def main():
-    # url = "https://books.toscrape.com/catalogue/the-mysterious-affair-at-styles-hercule-poirot-1_452/index.html"
-    # url_two = "https://books.toscrape.com/catalogue/misery_334/index.html"
-    # url_three = "https://books.toscrape.com/catalogue/misery_332/index.html"
-    # get_book_data(url)
-    # get_book_data(url_two)
-    # get_book_data(url_three)
+    url = "https://books.toscrape.com/index.html"
+    url_two = "https://books.toscrape.com/catalogue/misery_334/index.html"
+    url_three = "https://books.toscrape.com/catalogue/misery_332/index.html"
     category_url = "https://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
-    scrap_all_category_books(category_url)
+
+    # home url
+    scrap_datas(url)
+
+    # book url
+    scrap_datas(url_three)
+
+    # false url
+    scrap_datas(url_two)
+
+    # category url
+    scrap_datas(category_url)
 
 
 if __name__ == '__main__':
     main()
-
