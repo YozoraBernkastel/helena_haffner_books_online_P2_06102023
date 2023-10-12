ada2acf9412c03cb388cf2861962fd3b7445ff2f
Featherine <helaena.ha2fner@gmail.com>
2023-10-12 11:32:02 +0200

    #01 scrap book information function done

--- library/scrap_functions.py
+++ library/scrap_functions.py
@@ -1,6 +1,7 @@
 from library.http_functions import check_url
 from bs4 import BeautifulSoup
 import requests
+import re
 
 
 def get_book_data(url):
@@ -18,7 +19,28 @@ def get_book_data(url):
     book_data = {"product_page_url": url}
 
     soup = BeautifulSoup(url_page.text, 'html5lib')
-    book_data["universal_product_code"] = soup.find('td').text
+
+    # create an array containing all the td in the class "table table-striped" table ...
+    table_striped_table = soup.find("table", class_="table table-striped")
+    td_table = table_striped_table.find_all('td')
+
+    # then push each needed element in a dictionary in the right order.
+    book_data["universal_product_code"] = td_table[0].text
     book_data["title"] = soup.find('h1').text
+    book_data["price_including_tax"] = td_table[3].text
+    book_data["price_excluding_tax"] = td_table[2].text
+
+    # scrap the number only
+    book_data["number_available"] = re.findall('[0-9]+', td_table[5].text)
+
+    # scrap the description of the book in the metadata -> possibility to scrap it in the <article> tag with
+    # the class product_page too.
+    book_data["product_description"] = soup.find("meta", attrs={"name": "description"})["content"].strip()
+
+    # scrap the category which is in the <ul> tag with the class breadcrumb.
+    breadcrumb_class_table = soup.find("ul", class_="breadcrumb")
+    book_data["category"] = breadcrumb_class_table.find_all("a")[2].text
+
+    book_data["review_rating"] = td_table[6].text
     # à la place du return, on appellera la fonction d'export
     return book_data
