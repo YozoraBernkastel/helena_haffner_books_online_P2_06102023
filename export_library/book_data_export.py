feb4a90333dd5f82e8ddc1bab521cf6a3f36cba5
Featherine <helaena.ha2fner@gmail.com>
2023-10-12 14:47:40 +0200

    #02 create an outputs folder if there is no one + replace is False by not in if condition + remove useless is True in if condition

--- export_library/book_data_export.py
+++ export_library/book_data_export.py
@@ -1,11 +1,16 @@
+import os
 from os import path, stat
 import csv
 
 
 def book_data_export(data_dict: dict):
+
+    if not path.exists("../outputs"):
+        os.makedirs("../outputs")
+
     file_path = "../outputs/data_export.csv"
     # if the file doesn't exist or if it is empty, write the header by using the keys from data_dict
-    if path.exists(file_path) is False or stat(file_path).st_size == 0:
+    if not path.exists(file_path) or stat(file_path).st_size == 0:
         with open(file_path, 'w') as f:
             writer = csv.writer(f)
             writer.writerow((data_dict.keys()))
